9022fcd978b0edb0cfa8ca8b30ad18a809377c39
taborix98 <taborrisimone98@gmail.com>
2026-02-25 10:47:03 +0100

    Combattimento.py

--- combattimento.py
+++ combattimento.py
@@ -1,4 +1,4 @@
-#2. Simulatore di Combattimento a Turni
+#Simulatore di Combattimento a Turni
 #Descrizione:
 #Due o più personaggi combattono a turni, con punti vita, attacchi speciali, difesa, ecc.
 #Concetti usati:
@@ -17,16 +17,24 @@ class personaggio:
         #la difesa quando è 0 quando è minima e 100 quando è massima
         self.difesa= difesa  
         self.puntivita= puntivita
-    def attacca (self,avversario):
+    def attacca (self,avversario, personaggi, sono_io = False, io = None):
         avversario.puntivita-= self.attacco * (1.0- (avversario.difesa/100))
-        if avversario.puntivita < 0:
-            avversario.morto()
-        pass
+        if avversario.puntivita <= 0:
+            avversario.morto(personaggi)
+            if sono_io:
+                return False, -1
+        try:
+            return True , personaggi.index(io)
+        except:
+            return True, -1
     
-    def morto(self):
+    def morto(self, personaggi):        
         self.puntivita = 0
-        self.nome = f"{self.nome} (morto)"
 
+        personaggi.remove(self)  # Rimuove il personaggio dalla lista dei personaggi
+        self.nome = f"{self.nome} (morto)"
+        
+    
     def __str__(self):
         return f"{self.nome} - Attacco: {self.attacco}, Difesa: {self.difesa}, Punti Vita: {self.puntivita}"
 
@@ -44,31 +52,89 @@ class arciere(personaggio):
     def __str__(self):
         return super().__str__()
     
+class fragile(personaggio):
+    def __init__(self,nome):
+        super().__init__(nome,20,60,5)
+    def __str__(self):
+        return super().__str__()
+    
 class gioco:
-    def __init__(self):
+    def __init__(self, io = 0):
         self.personaggi= []
         self.personaggi.append(guerriero("thor"))
         self.personaggi.append(arciere("robin hood"))
         self.personaggi.append(guerriero("capitan america"))
         self.personaggi.append(arciere("guglielmo tell"))
+        self.personaggi.append(fragile("fabio"))
+        self.io = io
+        self.sono_vivo = True
         pass
+    def decidi_giocatore(self, io):
+        self.io = io
+
     def turno(self):
-        pass
         
+        for x in self.personaggi:
+
+            # Se il personaggio è lo stesso del giocatore            
+            if self.sono_vivo and (x == self.personaggi[io]):
+                # Gestisci il personaggio del giocatore
+                print(f"{x.nome}, è il tuo turno! Scegli un avversario da attaccare:")
+                avversario = int(input ())
+                while (avversario < 0 or avversario >= len(self.personaggi)) or (avversario == io):
+                    print("Avversario non valido, riprova.")
+                    print(f"{x.nome}, è il tuo turno! Scegli un avversario da attaccare:")
+                    avversario = int(input())
+                
+                print(f"{x.nome} attacca {self.personaggi[avversario].nome}!")
+                self.io = x.attacca(self.personaggi[avversario], self.personaggi, False, self.personaggi[io])[1]
+
+                self.stampa_personaggi()
+                    
+            else:
+                # Altrimenti gestisci il personaggio dal computer
+                import random 
+                avv = random.choice(self.personaggi)
+
+                while avv == x:
+                    print(x.nome, "ha provato ad attaccare se stesso, riprova.")
+                    avv = random.choice(self.personaggi) 
+                if (self.io != -1):
+                    temp = x.attacca(avv, self.personaggi, avv == self.personaggi[self.io], self.personaggi[self.io])
+                    self.sono_vivo = temp[0]
+                    self.io = temp[1]
+                    print(temp)
+                else:
+                    x.attacca(avv, self.personaggi)
+                
+                print(f"{x.nome} attacca {avv.nome}!")
+                self.stampa_personaggi()
+                
+                
+
     def stampa_personaggi(self):
-        print("thor")
         print("Dati personaggi: ")
         for x in self.personaggi:
+            print(self.personaggi.index(x), end=": ")
             print(x)
         print("\n")
 
-g = gioco()
 
-print("Inizio gioco: ")
+g = gioco ()
+print ("Simulatore di Combattimento a Turni")
+print ("=====================================")
+print ("Personaggi disponibili: ")
 g.stampa_personaggi()
-print("Primo turno: \n")
-g.turno()
-g.stampa_personaggi()
-print(" Secondo turno : \n")
-g.turno()
-g.stampa_personaggi()
+print("=====================================")
+print("Seleziona un personaggio per iniziare il combattimento: ")
+io = int(input())
+
+g.decidi_giocatore (io)
+
+while len(g.personaggi) > 1:
+    g.turno()
+
+print (g.personaggi[0].nome, "è il vincitore!")
+
+print("Il combattimento è terminato!")
+
